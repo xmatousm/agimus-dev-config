b10977f637ca5509b3ea487f609ae4a9d4d46565
Martin Matoušek <Martin.Matousek@cvut.cz>
2026-01-19 21:45:01 +0100

    Init.

---
+++ ws_build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Build workspace
+
+# Work in the directory of this script, prepare directories
+cd "$(dirname "$0")" || exit 255
+
+# Source ROS base
+# shellcheck source=/dev/null
+source "/opt/ros/$ROS_DISTRO/setup.bash"
+
+# Max number of CPU cores
+export MAKEFLAGS="-j 4"
+
+# Build the workspace. Merge install is required to make Python bindings work
+colcon build \
+       --symlink-install \
+       --merge-install \
+       --cmake-args \
+       --no-warn-unused-cli \
+       -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+       -DCMAKE_BUILD_TYPE=Release \
+       -DBUILD_TESTING=OFF \
+       -DBUILD_BENCHMARK=OFF \
+       -DBUILD_BENCHMARKS=OFF \
+       -DBUILD_EXAMPLES=OFF \
+       -DINSTALL_DOCUMENTATION=OFF \
+       -DBUILD_PYTHON_INTERFACE=ON \
+       -DGENERATE_PYTHON_STUBS=OFF \
+       -DCOAL_BACKWARD_COMPATIBILITY_WITH_HPP_FCL=ON \
+       -DCOAL_HAS_QHULL=ON \
+       -DBUILD_WITH_COLLISION_SUPPORT=ON \
+       -DBUILD_WITH_MULTITHREADS=ON \
+       "$@"
---
+++ ws_get.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Get all dependencies for Agimus demos
+
+# Work in the directory of this script, prepare directories
+cd "$(dirname "$0")" || exit 255
+mkdir -p src
+cd src || exit 255
+cd ..
+
+git config --global advice.detachedHead false
+
+# Get base repositories
+vcs import --recursive src < ws.repos || exit 255
+
+# Dependencies for Franka robots - not needed
+
+# Dependencies for KUKA robots
+vcs import --recursive src  < src/agimus-demos/kuka.repos || exit 255
+
+# Dependencies for optimal control - prebuilt
+
+# Agimus-specific dependencies
+vcs import --recursive src < src/agimus-demos/agimus_dev.repos || exit 255
+
+# shellcheck source=/dev/null
+source "/opt/ros/$ROS_DISTRO/setup.bash"
+
+# Install all dependencies that are available as binaries
+sudo apt-get update || exit 255
+
+rosdep update --rosdistro "$ROS_DISTRO" || exit 255
+
+rosdep install -y -i --from-paths src --rosdistro $ROS_DISTRO || exit 255
